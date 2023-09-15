f1e1882220ce9320d348dd1eca69bb744bd8d8df
Aashay-Thakur <thakur.aashay@gmail.com>
2023-09-16 00:16:04 +0530

    Priority Scheduling Done.

--- algorithms/non-pre-emptive/priority.js
+++ algorithms/non-pre-emptive/priority.js
@@ -6,10 +6,48 @@ export default function priority(data) {
 	// Sort process by priority
 	sortable
 		.sort(function (a, b) {
-			return a[1].priority - b[1].priority;
+			return a[1].arrivalTime - b[1].arrivalTime;
 		})
 		.sort(function (a, b) {
-			return a[1].arrivalTime - b[1].arrivalTime;
+			if (!a[1].arrivalTime - b[1].arrivalTime < 0) {
+				return a[1].priority - b[1].priority;
+			}
 		});
-	console.log(sortable);
+
+	var chartData = [];
+	var processedData = [];
+
+	sortable.forEach((process, index) => {
+		if (index === 0) {
+			process[1].startTime = process[1].arrivalTime;
+			process[1].endTime = process[1].burstTime + process[1].startTime;
+		} else {
+			process[1].startTime = process[1].arrivalTime <= sortable[index - 1][1].endTime ? sortable[index - 1][1].endTime : process[1].arrivalTime;
+			process[1].endTime = process[1].startTime + process[1].burstTime;
+		}
+		process[1].turnAroundTime = process[1].endTime - process[1].arrivalTime;
+		process[1].waitingTime = process[1].turnAroundTime - process[1].burstTime;
+
+		process[1].order = index + 1;
+		process[1].pid = Number(process[0].slice(1));
+
+		chartData.push({
+			label: process[0],
+			data: [
+				{
+					x: process[1].startTime,
+					y: 0,
+				},
+				{
+					x: process[1].endTime,
+					y: 0,
+				},
+			],
+		});
+
+		processedData.sort((a, b) => a[1].pid - b[1].pid);
+		processedData.push(process);
+	});
+
+	return { chartData, processedData, type: "Priority" };
 }
